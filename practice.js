6e190c3d46ee3deee43d5dc87565c60b31d115d9
DANIEL BULLOCK <DANIEL.I.BULLOCK@gmail.com>
2015-01-17 13:08:04 -0700

    "first attempt finished, all problems answered"

--- practice.js
+++ practice.js
@@ -331,7 +331,7 @@ of Data is to have an Array full of objects. */
 //Create an empty array called users.
 
   //Code Here
-
+var users = [];
 /*Now add three user objects to your users array. Each user object should contain the
 following properties. name, email, password, username.*/
 
@@ -345,6 +345,21 @@ var user1 = {
 
 //Your Code Here
 
+var user2 = {
+    name: 'Dan Bullock',
+    email: 'superbullock@gmail.com',
+    password: 'loser',
+    username: 'superbullock'
+}
+
+var user3 = {
+    name: 'Brian Stocking',
+    email: 'brianstocking@gmail.com',
+    password: 'scaredycat',
+    username: 'sirmeowsalot'
+}
+
+users.push(user1, user2, user3);    
 /*Now you have a very common data structure. Twitter is a good use case.
 It's easy to imagine that your followers list on Twitter is an Array full or objects
 and those objects contain properties about the specific person you follow.*/
@@ -354,6 +369,11 @@ objects until you find Tyler's account (use tylermcginnis33@gmail.com to find hi
 Once you find the particular indice he's located in, delete him from the array.*/
 
   //Code Here
+for (var i = users.length - 1; i >= 0; i--) {
+    if (users[i].email === 'tylermcginnis33@gmail.com') {
+        users.splice(i, 1);
+    }
+};
 
 //The activity we just did is very much how data works in 'the real world'.
 
